ebc163951bea354b74cf2c36f38ff2760e3d4a9e
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2024-03-04 14:54:29 -0600

    asd
    asd

--- MultiplayerJargon/MultplayerTest.gd
+++ MultiplayerJargon/MultplayerTest.gd
@@ -65,3 +65,19 @@ func change_level(scene: PackedScene):
 	# Add new level.
 	level.add_child(scene.instantiate())
 #endregion
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway!")
+
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
